ba3ae4fe6586427ae5e3b2b81a910fe8766357e8
Mikhail Andreev <mikhail.andreev@pemicro.com>
2014-07-29 12:26:08 -0400

    Day 6 165m total - Fixed the issue, now on trying to customize the menu.

--- TestApp/gen/com/example/testapp/R.java
+++ TestApp/gen/com/example/testapp/R.java
@@ -1091,6 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
+        public static final int action_search=0x7f05003e;
         public static final int action_settings=0x7f05003d;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
@@ -1200,6 +1201,7 @@ containing a value of this type.
     public static final class menu {
         public static final int display_message=0x7f0c0000;
         public static final int main=0x7f0c0001;
+        public static final int main_activity_actions=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1242,6 +1244,7 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int action_search=0x7f0a0013;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int button_send=0x7f0a0011;
